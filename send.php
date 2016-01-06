e9ac3a895730804c8d9dfb648bc4b4fa80bd961a
hetzner server <fate@jehy.ru>
2016-01-06 15:06:01 +0300

    added some error reporting

--- send.php
+++ send.php
@@ -1,16 +1,20 @@
 <?
 
-if($_REQUEST)
-{
-    require_once('db.inc');
-    $data=$_REQUEST['data'];
-    $data=json_decode($data,1);
-    #file_put_contents('1.txt',var_export($_REQUEST,1)."\n\n\n".var_export($data,1)."\n\n".json_last_error());
+if(!$_REQUEST['data'])
+    die(json_encode(['status'=>'error', 'msg'=>'no request data!']));
 
-        $sql = 'INSERT INTO data(added,sensor_id,val,ppm,ssid,ram) VALUES(NOW(),?,?,?,?,?)';
-        $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param('iiisi', $data['id'],$data['val'],$data['ppm'],$data['SSID'],$data['FreeRAM']);
-        $stmt->execute();
-
-}
+require_once('db.inc');
+$data=$_REQUEST['data'];
+$data=json_decode($data,1);
+if(!$data)    
+    die(json_encode(['status'=>'error', 'msg'=>json_last_error()]));
+#file_put_contents('1.txt',var_export($_REQUEST,1)."\n\n\n".var_export($data,1)."\n\n".json_last_error());
+$sql = 'INSERT INTO data(added,sensor_id,val,ppm,ssid,ram) VALUES(NOW(),?,?,?,?,?)';
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param('iiisi', $data['id'],$data['val'],$data['ppm'],$data['SSID'],$data['FreeRAM']);
+$res=$stmt->execute();
+if(res)
+    echo json_encode(['status'=>'ok']);
+else
+    echo json_encode(['status'=>'error', 'msg'=>$mysqli->error]);
 ?>
