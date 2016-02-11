0e0f430ed92a6f86e5cb4a20bc507e75594a3bcc
root <root@instance-1.c.my-server-1208.internal>
2016-02-11 15:00:56 +0300

    added script to create table

---
+++ co2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `data` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `added` datetime NOT NULL,
+  `sensor_id` int(11) unsigned NOT NULL,
+  `ppm` int(10) unsigned DEFAULT NULL,
+  `ssid` varchar(200) NOT NULL,
+  `ram` int(10) unsigned DEFAULT NULL,
+  `temp` int(10) unsigned DEFAULT NULL,
+  `humidity` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12669 DEFAULT CHARSET=utf8;
